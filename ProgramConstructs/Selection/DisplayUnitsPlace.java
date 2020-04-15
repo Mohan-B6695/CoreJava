d7cc3d7b2306b80f35c94a99d9be04c762107d95
MOHAN-B6695 <mohan.b600@gmail.com>
2020-04-16 01:38:08 +0530

    [Mohan]Added. Displaying units place using if else

---
+++ ProgramConstructs/Selection/DisplayUnitsPlace.java
@@ -0,0 +1,31 @@
+package Selection;
+
+import java.util.Scanner;
+
+public class DisplayUnitsPlace {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the input");
+        String value = scan.next();
+        switch (value.length()) {
+            case 1:
+                System.out.println("Units");
+                break;
+            case 2:
+                System.out.println("Tens");
+                break;
+            case 3:
+                System.out.println("Hundreds");
+                break;
+            case 4:
+                System.out.println("Thousands");
+                break;
+            case 5:
+                System.out.println("Ten-Thousand");
+                break;
+            default:
+                System.out.println("Infinity");
+                throw new IllegalArgumentException("Unexpected value: " + value);
+        }
+    }
+}
