9158ff15d705bc4ed82c03a4512e9bf0f09d8543
MOHAN-B6695 <mohan.b600@gmail.com>
2020-04-16 01:36:07 +0530

    [Mohan]Added. Displaying day of using if else

---
+++ ProgramConstructs/Selection/WeekDayDisplay.java
@@ -0,0 +1,27 @@
+package Selection;
+
+import java.util.Scanner;
+
+public class WeekDayDisplay {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Input");
+        int digit = scan.nextInt();
+        if (digit == 1)
+            System.out.println("Sunday");
+        else if (digit == 2)
+            System.out.println("Monday");
+        else if (digit == 3)
+            System.out.println("Tuesday");
+        else if (digit == 4)
+            System.out.println("Wednesday");
+        else if (digit == 5)
+            System.out.println("Thursday");
+        else if (digit == 6)
+            System.out.println("Friday");
+        else if (digit == 7)
+            System.out.println("Saturday");
+        else
+            System.out.println("Unexpected value: " + digit);
+    }
+}
